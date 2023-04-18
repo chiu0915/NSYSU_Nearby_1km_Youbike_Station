a3a93c122405e338cf883c339c8671de2fc4c6b9
邱冠智 <86599394+chiu0915@users.noreply.github.com>
2023-04-18 19:25:57 +0800

    Add files via upload

---
+++ TDX(1).py
@@ -0,0 +1,103 @@
+import requests
+import pandas as pd
+from datetime import datetime
+from pprint import pprint
+import json
+import time
+import datetime
+import csv
+import os
+
+client_id = 'b085040003-13ce90a3-9b9a-4ff2'
+client_secret = '9a142eeb-ddee-4e3c-9255-ca234ab716e2'
+
+token_url="https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token"
+AVAILABILITY_URL = "https://tdx.transportdata.tw/api/advanced/v2/Bike/Availability/NearBy?%24top=30&%24spatialFilter=nearby%2822.62752590909029%2C%20120.26465291318681%2C%201000%29&%24format=JSON"
+STATION_URL = "https://tdx.transportdata.tw/api/advanced/v2/Bike/Station/NearBy?%24top=30&%24spatialFilter=nearby%2822.62752590909029%2C%20120.26465291318681%2C%201000%29&%24format=JSON"
+
+class TDX():
+
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def get_token(self):
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
+        data = {
+            'grant_type': 'client_credentials',
+            'client_id': self.client_id,
+            'client_secret': self.client_secret
+        }
+        response = requests.post(token_url, headers=headers, data=data)
+        print("response.status_code:%d" % response.status_code)
+        # print(response.json())
+        return response.json()['access_token']
+
+    def get_response(self, url):
+        headers = {'authorization': f'Bearer {self.get_token()}'}
+        response = requests.get(url, headers=headers)
+        return response.json()
+
+if __name__ == '__main__':
+    while True:
+            try:
+                tdx = TDX(client_id, client_secret)
+                availability_response = tdx.get_response(AVAILABILITY_URL)
+                station_response = tdx.get_response(STATION_URL)
+                # print("國立中山大學幾何中心周圍1公里Youbike站點:")
+                # pprint(station_response)
+                # # print(type(station_response))
+                # print("國立中山大學幾何中心周圍1公里Youbike站點即時狀態:")
+                # pprint(availability_response)
+                # print(type(availability_response))
+
+                # 創建一個空的 DataFrame 用於存放比對後的資料
+                df_result = pd.DataFrame()
+
+                # 迭代 STATION_URL 的每一筆資料
+                for station in station_response:
+                    station_id = station['StationID']
+                    station_name = station['StationName']['Zh_tw']
+                    station_address = station['StationAddress']['Zh_tw']
+                    bikes_capacity = station['BikesCapacity']
+
+                    # 從 AVAILABILITY_URL 找出符合 StationID 的資料
+                    for avail in availability_response:
+                        if avail['StationID'] == station_id:
+                            available_rent_bikes = avail['AvailableRentBikes']
+                            available_rent_electricbikes = avail['AvailableRentBikesDetail']['ElectricBikes']
+                            available_rent_generalbikes = avail['AvailableRentBikesDetail']['GeneralBikes']
+                            available_return_bikes = avail['AvailableReturnBikes']
+                            update_time = avail['UpdateTime']
+                            break
+
+                    # 將比對後的資料加入 DataFrame
+                    df_result = df_result.append({
+                        'StationID': station_id,
+                        'StationName': station_name,
+                        'StationAddress': station_address,
+                        'BikesCapacity': bikes_capacity,
+                        'AvailableRentBikes': available_rent_bikes,
+                        'ElectricBikes':available_rent_electricbikes,
+                        'GeneralBikes':available_rent_generalbikes,
+                        'AvailableReturnBikes': available_return_bikes,
+                        'UpdateTime': update_time
+                    }, ignore_index=True)
+                print("國立中山大學幾何中心周圍1公里Youbike站點即時狀態:")
+                print(df_result)
+                # 設定要寫入的 CSV 檔名與路徑
+                csv_path = '國立中山大學幾何中心周圍1公里Youbike站點即時狀態.csv'
+                if os.path.isfile(csv_path):# 如果檔案已存在，就以 "a" 的模式打開 (append mode)
+                    df_result.to_csv(csv_path, mode='a', index=False)
+                else:# 如果檔案不存在，就創建一個新檔案並寫入欄位名稱
+                    df_result.to_csv(csv_path, mode='a', index=True)
+                print(f"Data collected at {datetime.datetime.now()}")
+                time.sleep(300)
+            except Exception as e:
+                # 印出錯誤訊息
+                print(f"Error occurred: {e}")
+                time.sleep(300)
+    
+
+        
+
